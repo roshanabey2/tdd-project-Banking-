79dd2e8252caffcd8ae53e15d40715fcfd37bb3a
Roshan Abeysekera <roshanabey2@gmail.com>
2023-04-21 18:23:18 +0100

    fixed typo, moded test_money.py into py file

--- py/test_money.py
+++ py/test_money.py
@@ -5,6 +5,6 @@ class TestMoney(unittest.TestCase):
         fiver = Dollar(5)
         tenner = fiver.times(2)
         self.assertEqual(10, tenner.amount)
-
-    if __name__=='__main__':
-        unittest.main()
+        
+if __name__=='__main__':
+    unittest.main()
