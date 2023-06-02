50848b5c2e4ed86a30a5f4dc9140995da8152202
Roshan Abeysekera <roshanabey2@gmail.com>
2023-06-02 16:33:09 +0100

    moved the Money and Porfolio constructors into different modules, refactored test code

--- js/test_money.js
+++ js/test_money.js
@@ -1,49 +1,36 @@
 const assert = require('assert');
-class Money {
-    constructor(amount, currency) {
-        this.amount = amount,
-        this.currency = currency
+const Money = require('./money');
+const Portfolio = require('./portfolio');
+
+
+class MoneyTest {
+    testMultiplication() {
+        let tenEuros = new Money(10, "EUR")
+        let twentyEuros = new Money(20, "EUR")
+        assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
     }
 
-    times(multiplier) {
-        return new Money(this.amount*multiplier, this.currency);
+    testDvision() {
+    let originalMoney = new Money(4002, "KRW")
+    let actualMoneyAfterDivision = originalMoney.divide(4)
+    let expectedMoneyAfterDivision = new Money(1000.5, "KRW")
+    assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
     }
-    divide(divisor) {
-        return new Money(this.amount / divisor, this.currency);
+
+    testAddition() {
+        let fiveDollars = new Money (5, "USD")
+        let tenDollars = new Money(10, "USD")
+        let fifteenDollars = new Money(15, "USD");
+        let portfolio = new Portfolio();
+        portfolio.add(fiveDollars, tenDollars)
+        assert.deepStrictEqual(fifteenDollars, portfolio.evaluate("USD"))
+    }
+
+    runAllTests() {
+        this.testMultiplication();
+        this.testDvision();
+        this.testAddition();
     }
 }
 
-class Portfolio {
-    constructor() {
-        this.moneys = [];
-    }
-    add(...moneys) {
-        this.moneys = this.moneys.concat(moneys);
-
-    }
-
-    evaluate(currency) {
-        let total = this.moneys.reduce ((sum, money) => {
-            return sum + money.amount
-        },0);
-        return new Money(total, currency)
-    }
-
-}
-
-let fiveDollars = new Money (5, "USD")
-let tenDollars = new Money(10, "USD")
-let tenEuros = new Money(10, "EUR")
-let twentyEuros = new Money(20, "EUR")
-let originalMoney = new Money(4002, "KRW")
-let actualMoneyAfterDivision = originalMoney.divide(4)
-let expectedMoneyAfterDivision = new Money(1000.5, "KRW")
-assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
-assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
-assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision)
-
-let fifteenDollars = new Money(15, "USD");
-let portfolio = new Portfolio();
-portfolio.add(fiveDollars, tenDollars)
-assert.deepStrictEqual(fifteenDollars, portfolio.evaluate("USD"))
-
+new MoneyTest().runAllTests();
